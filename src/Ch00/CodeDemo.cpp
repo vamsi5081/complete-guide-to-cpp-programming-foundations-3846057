79bd58e346f389648e571d854d5e3d30697e80b8
vamsi5081 <krishnavamsiduddupudi12@gmail.com>
2025-09-06 14:05:07 +0000

    multification in the code

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,21 +1,24 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 added multiplication
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result;
+    float num_1, num_2, result1, result2;
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
+    std::cout << "Enter number 2: ";
     std::cin >> num_2;
     
-    result = num_1 + num_2;
+    result1 = num_1 + num_2;
+
+    result2 = num_1 * num_2;
     
-    std::cout << "The result of the addition is " << result << std::endl;
-    
+    std::cout << "The result of the addition is " << result1 << std::endl;
+    std::cout << std::endl;
+    std::cout << "The result of the multiplication is " << result2 << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
